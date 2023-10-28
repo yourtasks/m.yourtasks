9a8433fcbd4a4209b379e220091fd4bcc50ab581
mdmofazzalhossain7 <mdmofazzalhossain789@gmail.com>
2023-10-28 20:22:15 +0600

    added manifest

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,6 +10,7 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 export const metadata = {
   title: "YourTasks - Get All Tasks in one place",
   description: "YourTasks is a dedicated social media for ToDo's",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }) {
